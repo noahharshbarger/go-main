e41ae8a2e02fba4c466d43e3192d6c9a67c1931a
Ian Christman <74085454+Fireboy19795@users.noreply.github.com>
2025-09-26 20:56:28 -0400

    Initial Commit

---
+++ src/sections/Session1-BasicForm/index.jsx
@@ -0,0 +1,65 @@
+"use client";
+import React, { useState } from 'react';
+
+
+const solarCalculatorBasic = () => {
+    const [inputs, setInputs] = useState({
+        monthlyBill: '',
+        homeSize: '',
+        electricityRate: 0.12, // Default rate
+    });
+
+    const handleInputChange = (field, value) => {
+            setInputs(prev => ({
+                ...prev,
+                [field]: value
+            }))
+    }
+    return (
+        <div className='space-y-6'>
+            <div>
+                <label className='block text-sm font-medium text-gray-700 mb-2'>
+                    Monthly Electricity Bill ($):
+                </label>
+                <input
+                    type='number' 
+                    value ={inputs.monthlyBill} 
+                    onChange={(e) = handleInputChange('monthlyBill', e.target.value)} 
+                    className='w-full px-3 py-2 border border-gray-300 rounded md focus:outline-none focus: ring-2 focus: ring-blue-500 text-black'
+                    placeholder='Enter your monthly bill'/>
+                    <p className='text-xs text-gray-500 mt-1'>
+                        Average U.S. household pays $117/month.
+                    </p>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700 mb-2'>
+                    Roof size in square feet:
+                </label>
+                <input
+                    type='number' 
+                    value ={inputs.homeSize} 
+                    onChange={(e) = handleInputChange('homeSize', e.target.value)} 
+                    className='w-full px-3 py-2 border border-gray-300 rounded md focus:outline-none focus: ring-2 focus: ring-blue-500 text-black'
+                    placeholder='Enter your monthly bill'/>
+                    <p className='text-xs text-gray-500 mt-1'>
+                        Average U.S. household roof is 1,500-2,000 sq ft.
+                    </p>
+            </div>
+            <div>
+                <label className='block text-sm font-medium text-gray-700 mb-2'>
+                    Electricity Rate ($/kWh):
+                </label>
+                <input
+                    type='number' 
+                    value ={inputs.electricityRate} 
+                    onChange={(e) = handleInputChange('electricityRate', e.target.value)} 
+                    className='w-full px-3 py-2 border border-gray-300 rounded md focus:outline-none focus: ring-2 focus: ring-blue-500 text-black'
+                    placeholder='Enter your monthly bill'/>
+                    <p className='text-xs text-gray-500 mt-1'>
+                        Average U.S. electricity rate is $0.12/kWh.
+                    </p>
+            </div>
+        </div>
+    );
+
+}
